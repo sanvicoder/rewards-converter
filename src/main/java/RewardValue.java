75c158a3485faf371c1beeeb40fb3377a412c4a9
Sanvi Agarwal <sanviagarwal2608@gmail.com>
2024-06-26 19:41:50 +0530

    added RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    double value;
+
+    RewardValue(double value) {
+        this.value = value;
+    }
+
+    public RewardValue(int milesValue) {
+        this.value = convertToCash(milesValue);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    public int getMilesValue() {
+        return (int) (value / 0.0035);
+    }
+
+    public double getCashValue() {
+        return value;
+    }
 }
